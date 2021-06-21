77154a0283def9e46cb46be9e263c18fe75bce6b
Juliano Wickboldt <julianowick@gmail.com>
2021-06-21 16:18:26 -0300

    Now replay script sends messages to an MQTT broker

--- replay.py
+++ replay.py
@@ -2,37 +2,64 @@ import os
 import sys
 import csv
 import time
+import json
 import datetime
 import logging
 from threading import Thread
+from paho.mqtt import client as mqtt_client
 
 logger_format = '%(asctime)s:%(threadName)s:%(message)s'
-logging.basicConfig(format=logger_format, level=logging.INFO, datefmt="%H:%M:%S")
+logging.basicConfig(format=logger_format, level=logging.DEBUG, datefmt="%H:%M:%S")
 
-if len(sys.argv) != 2:
-    print("Usage:", sys.argv[0], "<path_to_csv_files>")
-    exit(0)
+# MQTT config
+mqtt_broker = 'localhost'
+mqtt_port = 1883
+#mqtt_username = 'emqx'
+#mqtt_password = 'public'
 
-base_dir = sys.argv[1]
+# data config 
+data_dir = "data/" # folder with CSV data files (with trailing slash)
+starting_time = datetime.datetime(2019, 4, 1) # must be set to a time before any sensored data items
 
-class Event():
-    def __init__(self, key, value, timestamp):
-        self.key = key
-        self.value = value
-        self.timestamp = timestamp
+# connects to MQTT broker
+def connect_mqtt(client_id):
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            logging.info("Connected to MQTT Broker!")
+        else:
+            logging.info("Failed to connect, return code %d\n", rc)
+    
+    # set connecting client ID
+    client = mqtt_client.Client(client_id)
+    #client.username_pw_set(mqtt_username, mqtt_password)
+    client.on_connect = on_connect
+    client.connect(mqtt_broker, mqtt_port)
+    return client
 
-    def __str__(self):
-        return self.key + ": " + str(self.timestamp) + " - " + self.value
-
+# publishes sensored data from file to MQTT broker
 def sensor(filename):
-    logging.info(f"{filename} started")
-    #start = datetime.datetime.now() # TODO: take starting point as an argument later
-    with open(base_dir + filename, "r") as csv_file:
+    # filename will be used as key/topic
+    key = os.path.basename(filename)[0:-4] 
+    logging.info(f"sensor {key} started")
+    
+    #start = datetime.datetime.now()
+    # read data from CSV datafile
+    with open(data_dir + filename, "r") as csv_file:
         csv_reader = csv.reader(csv_file)
         headers = next(csv_reader)
-        key = os.path.basename(filename)[0:-4] # filename will be used as key
-        previous_ts = None
+        if len(headers) != 2: # expected exactly two columns (timestamp, value)
+            logging.warning("Invalid CSV file format!")
+            return
+
+        # connect to MQTT broker
+        client = connect_mqtt(key)
+        client.loop_start()
+
+        # set first "previous" timestamp
+        previous_ts = starting_time
         count = 0
+        topic = "/sensor/" + key
+        # every row will be sent as an 'event'
         for row in csv_reader:
             if len(row[0]) == 26: # timestamp with milliseconds
                 ts = datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S.%f")
@@ -40,30 +67,49 @@ def sensor(filename):
                 ts = datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S")
             else:
                 logging.waring(f"Invalid timestamp: {row[0]}")
-                continue
+                continue # next row
             
-            v = row[1]
-            e = Event(key, v, ts)
+            # the value should be usually a float
+            try:
+                value = float(row[1])
+            except:
+                value = row[1] # some other type will go as string
+            # json encoded timestamp (row[0]), value (row[1]) pair
+            isotime = ts.isoformat()
+            msg = json.dumps({headers[0]: isotime, headers[1]: value})
             
-            if previous_ts != None:
-                delta = ts - previous_ts
-                time.sleep(delta.total_seconds())
+            # sleeps an accorging to the interval between events/rows
+            delta = ts - previous_ts
+            # sleeps just before sending the next event
+            sleep = delta.total_seconds()
+            logging.debug(f"Sleeping for {sleep} seconds...")
+            time.sleep(sleep)
 
-            logging.info(e)
-            previous_ts = ts
+            result = client.publish(topic, msg)
             count += 1
-            if count >= 3:
-                break
+            # result: [0, 1]
+            status = result[0]
+            if status == 0: # success
+                logging.info(f"Sent `{msg}` on topic `{topic}` (count {count})")
+            else: # failure
+                logging.warning(f"Failed to send message `{msg}` on topic {topic} (count {count})")
+            
+            # update previous timestamp
+            previous_ts = ts
+            #if count >= 3: # break after N messages
+            #    break # temporary
     
-    logging.info(f"{filename} finished")
+    logging.info(f"sensor {key} finished")
 
+# start all sensor threads
 def main():
     logging.info("Main started")
-    threads = [Thread(target=sensor, args=(file, )) for file in os.listdir(path=base_dir)]
+    # every sensor will run as a separate thread
+    threads = [Thread(target=sensor, args=(filename, )) for filename in os.listdir(path=data_dir)]
     for thread in threads:
-        thread.start()
+        thread.start() # start every sensor
     for thread in threads:
-        thread.join() # waits for thread to complete its task
+        thread.join() # waits for sensor to finish sending data
     logging.info("Main Ended")
 
 # go
